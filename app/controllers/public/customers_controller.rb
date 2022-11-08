fdfb915cd0fe2ab49e137c2a46307dafdc3b9ee5
sonoda03 <proyou2022@gmail.com>
2022-11-08 08:44:58 +0000

    [Add]public/customersのcontroller編集＆追加、routeにも追加

---
+++ app/controllers/public/customers_controller.rb
@@ -0,0 +1,38 @@
+class Public::CustomersController < ApplicationController
+  # before_action :authenticate_customer!
+
+  def show
+    @customer = current_customer
+  end
+
+  def edit
+    @customer = current_customer
+  end
+
+  def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to mypage_path, notice: '会員情報を更新しました。'
+    else
+      render 'edit'
+    end
+  end
+
+  def unsubscribe
+    @customer = current_customer
+  end
+
+  def withdraw
+    @customer = current_customer
+    @customer.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :name_kana, :nickname, :email, :introduction)
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,26 @@
 Rails.application.routes.draw do
+
+  namespace :public do
+  end
   
   root :to =>"public/homes#top"
-  
+
   # 顧客用
   # URL /customers/sign_in ...
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  
+
   # 顧客用
   scope module: :public do
-    
+    # 顧客側のマイページ,登録情報編集&更新
+    get 'customers/my_page' => 'customers#show'
+    get 'customers/information/edit' => 'customers#edit'
+    patch 'customers/infomation' => 'customers#update'
+    # 顧客側の退会確認画面,退会処理
+    get '/customers/unsubscribe' => 'customers#unsubscribe'
+    patch '/customers/withdraw' => 'customers#withdraw'
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
