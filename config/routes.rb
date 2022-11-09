a55a26ae408fd61fd56ef65ab7ac5e2c1e3fe755
sonoda03 <proyou2022@gmail.com>
2022-11-09 07:13:53 +0000

    [Add]管理者ログイン機能＆画面の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
+  root :to =>"public/homes#top"
 
   namespace :public do
   end
 
-  root :to =>"public/homes#top"
-
   # 顧客用
-  # URL /customers/sign_in ...
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
@@ -25,6 +23,13 @@ Rails.application.routes.draw do
     get '/customers/unsubscribe' => 'customers#unsubscribe'
     patch '/customers/withdraw' => 'customers#withdraw'
   end
+
+  # 管理者用
+  # URL /admin/sign_in
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
