2db9545700c6b6345287a927abf6cb853d6fb7b7
sonoda03 <proyou2022@gmail.com>
2022-11-12 17:05:10 +0000

    [Add]public/usersの機能&View追加(サイドバーはなし)

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,10 +1,38 @@
 class Public::UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      flash[:notice] = '会員情報を更新しました。'
+      redirect_to user_path
+    else
+      render 'edit'
+    end
   end
 
   def unsubscribe
+    @user = current_user
+  end
+
+  def withdraw
+    @user = current_user
+    @user.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :first_name_kana, :last_name_kana, :nickname, :email, :introduction, :profile_image)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :profile_image
+
+  def self.guest
+    find_or_create_by!(email: 'guest0123@example.com') do |user|
+      user.last_name = '閲覧'
+      user.first_name = '太郎'
+      user.last_name_kana = 'エツラン'
+      user.first_name_kana = 'タロウ'
+      user.nickname = 'ゲスト'
+      user.introduction = '運動による休憩方法を積極的に投稿しております！！'
+      user.password = SecureRandom.urlsafe_base64
+    end
+  end
 end
