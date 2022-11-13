b675728a3ed7903c6a0281af3a40188a3c54c3ba
sonoda03 <proyou2022@gmail.com>
2022-11-13 07:59:45 +0000

    [Add]タグ編集機能追加

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -30,11 +30,20 @@ class Public::PostsController < ApplicationController
 
   def edit
     @post = Post.find(params[:id])
+    @tag_list=@post.tags.pluck(:name).join(',')
   end
 
   def update
     @post = Post.find(params[:id])
+    tag_list = params[:post][:name].split(',')
     if @post.update(post_params)
+      # 投稿についているタグを一旦全て削徐
+      @old_relations=PostTag.where(post_id: @post.id)
+      @old_relations.each do |relation|
+        relation.delete
+      end
+      # 再度タグ登録
+      @post.save_tag(tag_list)
       flash[:notice] = "投稿を更新しました。"
       redirect_to post_path(@post)
     else
