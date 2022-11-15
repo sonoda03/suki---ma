1eb33045bb26aca2ca9f4082fcc70893d1f3cf0e
sonoda03 <proyou2022@gmail.com>
2022-11-15 19:35:55 +0000

    [Change]ゲストログイン後の遷移先をTopから全投稿一覧へ変更

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -35,7 +35,7 @@ class Public::SessionsController < Devise::SessionsController
   def guest_sign_in
     user = User.guest
     sign_in user
-    redirect_to root_path
+    redirect_to posts_path
   end
 
   # ログアウト
