2b69217be005958a9b99ca0292f69d780e282c89
sonoda03 <proyou2022@gmail.com>
2022-11-21 09:15:08 +0000

    [Delete]をUsesテーブルのis_deleted:boolean(カラム名)を削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_21_090403) do
+ActiveRecord::Schema.define(version: 2022_11_21_091007) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -111,7 +111,6 @@ ActiveRecord::Schema.define(version: 2022_11_21_090403) do
     t.string "first_name_kana", null: false
     t.string "nickname", null: false
     t.text "introduction"
-    t.boolean "is_deleted", default: false, null: false
     t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
