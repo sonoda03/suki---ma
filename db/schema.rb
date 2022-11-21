0833dbe14496df97a6337c06752c470913a29439
sonoda03 <proyou2022@gmail.com>
2022-11-21 09:06:42 +0000

    [Add]管理者がユーザを削除するためのadmin(カラム名)をUsesテーブルへ追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_13_140443) do
+ActiveRecord::Schema.define(version: 2022_11_21_090403) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -112,6 +112,7 @@ ActiveRecord::Schema.define(version: 2022_11_13_140443) do
     t.string "nickname", null: false
     t.text "introduction"
     t.boolean "is_deleted", default: false, null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
