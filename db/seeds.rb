d33b1da3b794c04788067518ede505979359e8f6
sonoda03 <proyou2022@gmail.com>
2022-11-25 09:41:34 +0000

    [Fix]テストデータのタグ&タグ中間テーブル完成

--- db/seeds.rb
+++ db/seeds.rb
@@ -182,14 +182,34 @@ Post.create!(
   ]
 )
 
+
+
+
+
 Tag.create!(
   [
+    {name: 'ストレッチ'},
+    {name: '自宅'},
+    {name: '瞑想'},
+    {name: 'リラックス'},
     {name: '10分'},
     {name: 'クラシック'},
     {name: 'テンションアップ'},
+    {name: '落ち着く'},
+    {name: 'ハープ'},
+    {name: '短め'},
     {name: '5分'},
+    {name: 'かわいい'},
     {name: '甘い'},
-    {name: 'スイーツ'}
+    {name: 'スイーツ'},
+    {name: 'ドーナッツ'},
+    {name: 'ゼリー'},
+    {name: '一瞬'},
+    {name: '首コリ'},
+    {name: 'いつでも'},
+    {name: '疲れ'},
+    {name: 'ツボ'},
+    {name: '外'}
   ]
 )
 
@@ -197,11 +217,42 @@ PostTag.create!(
   [
     {post_id: 1, tag_id: 1},
     {post_id: 1, tag_id: 2},
-    {post_id: 1, tag_id: 3},
+    {post_id: 1, tag_id: 10},
+
+    {post_id: 2, tag_id: 2},
+    {post_id: 2, tag_id: 3},
     {post_id: 2, tag_id: 4},
     {post_id: 2, tag_id: 5},
-    {post_id: 2, tag_id: 6},
-    {post_id: 2, tag_id: 3},
+
+    {post_id: 3, tag_id: 4},
+    {post_id: 3, tag_id: 12},
+
+    {post_id: 4, tag_id: 5},
+    {post_id: 4, tag_id: 6},
+    {post_id: 4, tag_id: 7},
+
+    {post_id: 5, tag_id: 8},
+    {post_id: 5, tag_id: 9},
+    {post_id: 5, tag_id: 10},
+
+    {post_id: 6, tag_id: 11},
+    {post_id: 6, tag_id: 13},
+    {post_id: 6, tag_id: 14},
+    {post_id: 6, tag_id: 15},
+    {post_id: 6, tag_id: 17},
+
+    {post_id: 7, tag_id: 11},
+    {post_id: 7, tag_id: 13},
+    {post_id: 7, tag_id: 16},
+    {post_id: 7, tag_id: 17},
+
+    {post_id: 8, tag_id: 2},
+    {post_id: 8, tag_id: 17},
+    {post_id: 8, tag_id: 18},
+    {post_id: 8, tag_id: 19},
+    {post_id: 8, tag_id: 20},
+    {post_id: 8, tag_id: 21},
+    {post_id: 8, tag_id: 22},
   ]
 )
 
@@ -211,17 +262,22 @@ Favorite.create!(
     {user_id: 1, post_id: 4},
     {user_id: 1, post_id: 6},
     {user_id: 1, post_id: 8},
+
     {user_id: 2, post_id: 1},
     {user_id: 2, post_id: 7},
     {user_id: 2, post_id: 8},
+
     {user_id: 3, post_id: 1},
     {user_id: 3, post_id: 3},
+
     {user_id: 4, post_id: 1},
     {user_id: 4, post_id: 2},
     {user_id: 4, post_id: 3},
     {user_id: 4, post_id: 4},
     {user_id: 4, post_id: 5},
+
     {user_id: 5, post_id: 3},
+
     {user_id: 6, post_id: 7},
     {user_id: 6, post_id: 1},
     {user_id: 6, post_id: 3}
